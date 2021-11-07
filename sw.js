ab4f632bad40d3f94b975dc6e254be6295e3b2eb
shaunakmukherjee <shaunak.mukherjee94@gmail.com>
2021-11-07 20:01:01 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b37bd3b016943b0182f5.js"
+    "url": "webpack-runtime-b639145795bd2c9436b6.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "63a2f4bcb51777999f2e7dd6e61c30b0"
+    "revision": "f065efe57904b03edaab1e9ff27584f0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8f4ecebcbe0bf4e8930f933c533e580f"
+    "revision": "0db37cbae63babdcad68c55d31e11c40"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
