08f8e9779ea65016815f5dc20b7265206756befb
shaunakmukherjee <shaunak.mukherjee94@gmail.com>
2026-01-15 12:16:36 +0000

    Updates

--- _next/static/w_tFi0GVkcqBvKCGJBDpp/_buildManifest.js
+++ _next/static/w_tFi0GVkcqBvKCGJBDpp/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shaunakmukherjee.github.io//_next/:path+",
+        "destination": "/shaunakmukherjee.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
