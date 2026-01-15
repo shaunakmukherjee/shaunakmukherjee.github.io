3cb56e87f3a4aec1991deeef3c735bb833ec5991
shaunakmukherjee <shaunak.mukherjee94@gmail.com>
2026-01-15 12:15:50 +0000

    small nextjs fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production"
 const nextConfig = {
   output: "export",
   basePath: isProd ? "/shaunakmukherjee.github.io" : "",
+  assetPrefix: isProd ? "/shaunakmukherjee.github.io/" : "",
   images: {
     unoptimized: true,
   },
