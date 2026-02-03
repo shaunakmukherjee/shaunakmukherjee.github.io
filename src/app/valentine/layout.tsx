c51733ccdf101d7b1ffa0583ac478d3a6218ba79
shaunakmukherjee <shaunak.mukherjee94@gmail.com>
2026-02-03 10:38:14 +0000

    ahem ahem

---
+++ src/app/valentine/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "Valentine ✨",
+  robots: {
+    index: false,
+    follow: false,
+  },
+}
+
+export default function ValentineLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
